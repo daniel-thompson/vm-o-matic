41a078db1683028925052819f2a93584a2a08ba0
Daniel Thompson <daniel@redfelineninja.org.uk>
2023-06-14 10:08:21 +0100

    riscv64: Remove dead code
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- mk/riscv64.mk
+++ mk/riscv64.mk
@@ -5,13 +5,6 @@
 # Look up the TOPDIR using our location in the tree as a reference
 TOPDIR = $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
-# This assumes a Debian-derived host OS with qemu-efi-aarch64 installed.
-# It will not work out-of-the-box on other systems. If your distro
-# doesn't have this package, or equivalent, then it is possible to
-# extract the file from the raw .deb package.
-# See: https://packages.debian.org/testing/qemu-efi-arm .
-FIRMWARE = file:///usr/share/AAVMF/AAVMF_CODE.fd
-
 QEMU = qemu-system-riscv64
 MACHINE_FLAGS = -cpu rv64 -M virt -smp $(VM_CPUS) -m $(VM_RAMSIZE_MB) -nographic
 BIOS_FLAGS = -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf \
