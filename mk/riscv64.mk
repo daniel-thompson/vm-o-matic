5a6fe91e3bed8a2662c993035ea21d1d2ca78328
Daniel Thompson <daniel@redfelineninja.org.uk>
2026-01-07 10:16:15 +0000

    mk: riscv64: Fix for latest versions of Debian
    fw_jump.elf can no longer be loaded alongside uboot.elf. Happily we can
    simply switch to fw_jump.bin.

--- mk/riscv64.mk
+++ mk/riscv64.mk
@@ -7,10 +7,13 @@ TOPDIR = $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
 QEMU = qemu-system-riscv64
 MACHINE_FLAGS = -cpu rv64 -M virt -smp $(VM_CPUS) -m $(VM_RAMSIZE_MB) -nographic
-BIOS_FLAGS = -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf \
-	     -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf
+
+BIOS_FLAGS = -bios $(OPENSBI) -kernel $(UBOOT)
 NETWORK_FLAGS = -device virtio-net-device,netdev=net \
 		-netdev user,id=net,hostfwd=tcp::$(VM_SSH)-:22
 HEADLESS_FLAGS =
+
+OPENSBI = /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.bin
+UBOOT = /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf
 
 include $(TOPDIR)/mk/generic.mk
